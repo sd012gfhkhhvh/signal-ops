d32afd78bc9e03f18adfd8c91b276e9f2310294f
Soham Das <sohamdas.nest@gmail.com>
2026-07-09 02:46:38 +0530

    refactor: user entity

--- apps/nest-app/src/users/entities/user.entity.ts
+++ apps/nest-app/src/users/entities/user.entity.ts
@@ -2,14 +2,14 @@ import { Column, Entity, PrimaryColumn } from 'typeorm';
 @Entity()
 export class User {
   @PrimaryColumn({ type: 'uuid', generated: 'uuid' })
-  id!: string;
+  id: string;
 
   @Column({ type: 'varchar', length: 255 })
-  name!: string;
+  name: string;
 
   @Column({ type: 'varchar', length: 255, unique: true })
-  email!: string;
+  email: string;
 
   @Column({ type: 'varchar', length: 255 })
-  password!: string;
+  password: string;
 }
